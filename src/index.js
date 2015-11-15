14733fb77b3c243aa5e7a01ed179fac34e71538f
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-11-15 16:36:56 +0530

    add bar chart data

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-
 import {Charts} from './chartexm.js';
 import React from 'react';
-import BarChart from './barchart.js';
 import {CommentBox} from './Comments'
 import { render } from 'react-dom';
-
-
+import _ from 'underscore';
+import {LineChart} from './forecast.js';
 //render(<KeysExample results={[ {id : 1, text : 'helloububg'}, {id : 2, text : 'world'}, {id: 3, text : 'universe'}]} />,
 //     document.getElementById("root"));
 
 // render(<PopChart/>,document.getElementById("root"));
 
-
 var linedata = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["2010", "2012", "2013", "2014", "2015"],
     datasets: [
         {}
     ]
 };
 
-//render(<BarChart linedata={linedata}/>,document.getElementById("root"));
-//render(<CommentBox data={data}/>,document.getElementById("root"));
-render(<Charts data={linedata}></Charts>,document.getElementById("root"));
+var line1data = {
+    labels: ["rural","urban","rural_urban"],
+    datasets: [
+        {}
+    ]
+};
+
+
+//render(<Charts data={linedata}></Charts>,document.getElementById("root"));
+render(<LineChart data={linedata} bdata={line1data}></LineChart>,document.getElementById("root"));
